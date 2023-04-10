76c6a178edd6bee7b3bd305b8fc0cd3d4203ed83
IamGOOD <rukshenas1990@gmail.com>
2023-04-09 23:38:40 -0400

    Add models

--- cook/blog/models.py
+++ cook/blog/models.py
@@ -1,16 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    parent = TreeForeignKey(
-        'self',
-        related_name='children',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    parent = TreeForeignKey('self', related_name='children', on_delete=models.SET_NULL, null=True, blank=True)
 
 
     class MPTTMeta:
@@ -20,3 +15,35 @@ class Category(MPTTModel):
 class Tag(MPTTModel):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='articles/')
+    category = models.ForeignKey(Category, related_name='post', on_delete=models.SET_NULL, null=True)
+    text = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='post')
+    create_at = models.DateTimeField(auto_now_add=True)
+
+class Resipe(models.Model):
+    name = models.CharField(max_length=100)
+    serves = models.CharField(max_length=50)
+    prep_time = models.PositiveIntegerField(default=0)
+    cook_time = models.PositiveIntegerField(default=0)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        related_name='recipe',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+    message = models.CharField(max_length=500)
+    post = models.ForeignKey(Post, related_name='comment', on_delete=models.CASCADE)
