f8850dacb6f71bd5b04f991b64d15e8becb2229e
IamGOOD <rukshenas1990@gmail.com>
2023-04-09 23:51:43 -0400

    Add superuser + registration models

--- cook/blog/admin.py
+++ cook/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+# name: Jack
+# mail: Jack@gmail.com
+# pas: J123123123
+
+admin.site.register(models.Post)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Resipe)
+admin.site.register(models.Comment)
